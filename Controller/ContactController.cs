03b3a5d8d2720483318bcd61ba724422428251c5
Alexander <Ciga_fu80@student.itstep.org>
2022-05-16 19:36:23 +0300

    Add contact controller

---
+++ Controller/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using ContactsMVC.Model;
+using System;
+using System.Collections.Generic;
+
+namespace ContactsMVC.Controller
+{
+    public class ContactController
+    {
+        private List<Contact> _contacts;
+
+        public ContactController()
+        {
+            _contacts = new List<Contact>();
+        }
+        public ContactController(List<Contact> contacts)
+        {
+            _contacts = new List<Contact>(contacts);
+        }
+
+        public void AddContact(Contact c)
+        {
+            _contacts.Add(c);
+        }
+        public void RemoveContact(Contact c)
+        {
+            if(_contacts.Contains(c))
+                _contacts.Remove(c);    
+        }
+
+        public List<Contact> GetContacts()
+        {
+            return _contacts;
+        }
+    }
+}
