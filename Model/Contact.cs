7ed15f4bc06a339dc902f9b96d287d3b17d2d74e
Alexander <Ciga_fu80@student.itstep.org>
2022-05-16 19:27:07 +0300

    Add contact model

---
+++ Model/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsMVC.Model
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public List<string> Numbers { get; set; }
+
+        public Contact()
+        {
+            Name = LastName = Adress = String.Empty;
+            Numbers = new List<string>();
+        }
+
+        public Contact(string name, string lastName, string adress,List<string> numbers)
+        {
+            Name = name;
+            LastName = lastName;
+            Adress = adress;
+            Numbers = new List<string>(numbers);
+        }
+
+    }
+}
--- View/MainForm.Designer.cs
+++ View/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace ContactsMVC
+﻿namespace ContactsMVC.View
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
